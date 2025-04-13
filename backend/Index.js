f3fc8e52f6a7387ce820e2786969a85da9d51120
uNnAtI1314 <43136649+unnati6@users.noreply.github.com>
2025-04-13 13:35:34 +0530

    Switched to MongoDB Atlas

--- backend/Index.js
+++ backend/Index.js
@@ -1,20 +1,21 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
-dotenv.config();
+
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import router from './Routes/UserRouter.js';
 import linkRouter from './Routes/LinkRouter.js';
 const PORT = process.env.PORT || 5000;
 const app = express();
-
+const db  = "mongodb+srv://unnati:unnati12345@cluster0.m35bgfm.mongodb.net/userinfo?retryWrites=true&w=majority&appName=Cluster0";
+dotenv.config();
 app.use(bodyParser.json());
 app.use(cors());
 app.listen(PORT,()=>{
     console.log(`${PORT} is connect`)
 })
-mongoose.connect('mongodb://localhost:27017/UserLog')
+mongoose.connect(db)
 .then(()=>{
     console.log("Database Connect")
 })
