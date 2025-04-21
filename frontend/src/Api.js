f3e4f820358aed2e5979cab156ab9f2390e2cce0
uNnAtI1314 <43136649+unnati6@users.noreply.github.com>
2025-04-21 15:06:01 +0530

    added environment variable support for baseURL

--- frontend/src/Api.js
+++ frontend/src/Api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: process.env.REACT_APP_API_BASE_URL,
+const API = axios.create({ baseURL: 'https://userauth-backend-p1f2.onrender.com/'
  }
   
 );
