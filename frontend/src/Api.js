dff74a34114ee43783cbd0e567b9dfcd82d75c78
uNnAtI1314 <43136649+unnati6@users.noreply.github.com>
2025-04-21 14:48:28 +0530

    changed API baseURL to render live url

--- frontend/src/Api.js
+++ frontend/src/Api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'http://localhost:5000/'
+const API = axios.create({ baseURL: 'https://userauth-backend-p1f2.onrender.com/'
  }
   
 );
